f4ed08d5992227c0d6e3817662ac20ea482ab1fc
Connor Moriarty <strandableyt@gmail.com>
2020-12-01 19:53:05 +1030

    addition of logo to navbar

--- guide/.vuepress/config.js
+++ guide/.vuepress/config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const sidebar = require('./components/Sidebar.js');
 const nav = require('./components/Navbar.js');
 const head = require('./components/Head.js');
@@ -27,39 +26,6 @@ const config = {
         editLinks: true,
         docsBranch: 'master',
         editLinkText: 'Contribute on Github!',
-        startYear: '2018'
-    }
-};
-
-module.exports = config;
-=======
-const sidebar = require('./components/Sidebar.js');
-const nav = require('./components/Navbar.js');
-const head = require('./components/Head.js');
-
-const config = {
-    title: 'MenuDocs Guide',
-    head,
-    theme: 'reco',
-    themeConfig: {
-        blogConfig: {
-            category: {
-              location: 2,
-              text: 'Guides'
-            }
-        },
-        author: 'MenuDocs Community',
-        noFoundPageByTencent: false,
-        nav,
-        sidebar,
-        smoothScroll: true,
-        sidebarDepth: 3,
-        search: false,
-        lastUpdated: 'Last Updated',
-        repo: 'menudocs/guides',
-        editLinks: true,
-        docsBranch: 'master',
-        editLinkText: 'Contribute on Github!',
         startYear: '2018',
         notFoundMessages: [
             `Fun Fact: MenuDocs was founded in 2018 by Connor.`,
@@ -71,4 +37,3 @@ const config = {
 };
 
 module.exports = config;
->>>>>>> master
